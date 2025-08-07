953472bd90a50be54311b86e5488677f185749b6
Rajeswara rao <rajeswararao688@gmail.com>
2025-08-07 14:31:21 +0000

    Task 3: Docker with Terraform

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx-tf"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
